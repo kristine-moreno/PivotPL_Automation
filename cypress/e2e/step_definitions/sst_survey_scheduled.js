9057cf6c7e687ed7228346e64c5729896cd61345
Abegail Sangalang <abegail.sangalang@tooltwist.com>
2022-10-27 17:57:42 +0800

    Rename some files

--- cypress/e2e/step_definitions/sst_survey_scheduled.js
+++ cypress/e2e/step_definitions/sst_survey_scheduled.js
@@ -1,9 +1,9 @@
 import { Given, When, And, But, Then } from "@badeball/cypress-cucumber-preprocessor";
 import { sstSurveyPO } from "../../page_objects/sstSurvey_PO";
 import { loginSelectionPO } from "../../page_objects/loginSelection_PO";
-import { surveyNotAvailablePO } from "../../page_objects/surveyNotAvailable_PO";
-const surveyId = require('../../fixtures/surveyLink/survey_not_available.json')
-const studentId = require('../../fixtures/studentsData/Student.json')
+import { schedSurvey } from "../../page_objects/schedSurvey_PO";
+const surveyId = require('../../fixtures/surveyLink/survey_scheduled.json')
+const studentId = require('../../fixtures/studentsData/closeSurveyStudent.json')
 
 const configs = Cypress.env();
 
@@ -16,6 +16,7 @@ When('I login my valid survey_id and student_id', () => {
     cy.get(sstSurveyPO.pivotLogo).should('be.visible');
     cy.get(sstSurveyPO.sstLabel).should('be.visible').contains('Student Survey on Teaching');
     cy.get(sstSurveyPO.sstSubLabel).should('be.visible').contains('Welcome to your student survey on teaching');
+    cy.get(sstSurveyPO.sstTalkLabel).should('be.visible').contains('Please talk to your teacher if you have forgotten your Student ID.');
 
     let sid = 0;
     surveyId.forEach(testSurvey=>{
@@ -30,23 +31,25 @@ When('I login my valid survey_id and student_id', () => {
 
             //checking Not Available page
             cy.log("And the survey is not open yet");
-            cy.get(surveyNotAvailablePO.notAvailablePivotLogo,{timeout:5000}).should('be.visible');
-            cy.get(surveyNotAvailablePO.wereSorryLabel,{timeout:5000}).should('be.visible');
-            cy.get(surveyNotAvailablePO.notOpenLabel,{timeout:5000}).should('be.visible');
-            cy.get(surveyNotAvailablePO.centerImage,{timeout:5000}).should('be.visible');
-            cy.get(surveyNotAvailablePO.notifOpenLabel,{timeout:5000}).should('be.visible');
-            cy.get(surveyNotAvailablePO.notAvailableLogout,{timeout:5000}).should('be.visible');
+            cy.get(schedSurvey.notAvailablePivotLogo,{timeout:5000}).should('be.visible');
+
+            cy.get(schedSurvey.wereSorryLabel,{timeout:5000}).should('be.visible').contains("We're sorry");;
+            cy.get(schedSurvey.notOpenLabel,{timeout:5000}).should('be.visible').contains("This survey is not open yet.");
+            cy.get(schedSurvey.centerImage,{timeout:5000}).should('be.visible');
+            cy.get(schedSurvey.notifOpenLabel,{timeout:5000}).should('be.visible').contains("Your teacher will let you know when it opens.");
+            cy.get(schedSurvey.notAvailableLogout,{timeout:5000}).should('be.visible');
             cy.log("Elements are available")
 
             //click logout button
-            cy.get(surveyNotAvailablePO.notAvailableLogout,{timeout:5000}).click();
+            cy.get(schedSurvey.notAvailableLogout,{timeout:5000}).contains("Log out").click();
             cy.log("Then I should be navigated to the Survey Not Available page");
             cy.log("And all elements should be properly displayed");
 
             //checking Login selection elements
             cy.log("And I should be able to log out my account");
             cy.get(loginSelectionPO.loginSelectpivotLogo,{timeout:5000}).should('be.visible');
-            cy.get(loginSelectionPO.makeSelectionLabel,{timeout:5000}).should('be.visible');
+            cy.get(loginSelectionPO.makeSelectionLabel,{timeout:5000}).should('be.visible').contains("Make your selection");
+
             cy.get(loginSelectionPO.selectWellbeingButton,{timeout:5000}).should('be.visible');
             cy.get(loginSelectionPO.selectSSTButton,{timeout:5000}).should('be.visible');
 
---
+++ cypress/page_objects/wbSurvey_PO copy.js
@@ -0,0 +1,33 @@
+export const sstSurveyPO = { 
+
+    //Login Page Elements
+    pivotLogo: 'img',
+    sstLabel:'.fzaoYV',
+    sstSubLabel:'.fwlSGW',
+    sstTalkLabel:'[class="sc-mfv47r-0 hLgsZj"]',
+    surveyID:'#survey-id',
+    studentID:'#student-id',
+    sstLoginBtn:'.MuiButton-label',
+
+    //Onboarding Elements
+    greetings:'.jPIHnR',
+    feedbackSubtext:'.kATjNo',
+    subtext:'.dyckeG',
+    sliderRightArrow:'[data-testid="onboarding-slider-right-arrow"] > .sc-15fa221-0 > img',
+    sliderRightArrow2:'[data-testid="onboarding-slider-right-arrow"] > .sc-15fa221-0 > img',
+    imgSlides:'.slick-active > :nth-child(1) > div > img',
+    startSurveyBtn:'[data-testid="app-button"]',
+    
+    //SST Survey Elements
+    studentName:'.ewzZu',
+    Q1_RadioGroup:'[data-testid="23-radio-group"]',
+    Q2_RadioGroup:'[data-testid="24-radio-group"]',
+    submitBtn:'[data-testid="app-button"]',
+
+    //Thank You Page Elements
+    congratsLabel:'[data-testid="thank-you-name"]',
+    thankyouTitle:'[data-testid="thank-you-title"]',
+    thankyouIcon:'[data-testid="thankyou_icon"]',
+    thankyouNew:'[data-testid="thank-you-new"]',
+    thankyouLogout:'[data-testid="thank-you-logout-button"]',
+}
